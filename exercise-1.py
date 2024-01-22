c8d88c1eb0658c7c3ae8497c9372f3cac42440bc
Andrew Alsing <andrew32alsing@gmail.com>
2024-01-22 00:57:32 -0800

    completed exercise 1

--- exercise-1.py
+++ exercise-1.py
@@ -10,16 +10,23 @@ Exercise 1
 # - What line number is causing the error?
 # - What can you deduce about the cause of the error?
 
+# Expected Output: The program should find and print the largest difference between consecutive numbers in the list [5, 3, 1, 2, 6, 4].
+# Actual Output: The code is incomplete and will raise an error.
+# Error Message: IndexError: list index out of range
+# Line Number Causing the Error: line 31, in find_largest_diff diff = abs(list_of_nums[i] - list_of_nums[i+1])
+# Cause of the Error: The code does not handle the last element of the list properly, which will result in an "IndexError" when trying to access list_of_nums[i+1] for the last element.
 
 # PART 2: State Assumptions
 #
 # TODO: State your assumptions here or say them out loud to your partner ...
 # Make sure to be SPECIFIC about what each of your assumptions is!
 
+# The code assumes that the list always contains at least two elements for calculating the difference and should be changed to stop at the second to last index.
+
 def find_largest_diff(list_of_nums):
     """Find the largest difference between *consecutive* numbers in a list."""
     largest_diff = 0
-    for i in range(len(list_of_nums)):
+    for i in range(len(list_of_nums)): # fix by stopping at second to last index with: for i in range(len(list_of_nums) - 1):
         diff = abs(list_of_nums[i] - list_of_nums[i+1])
         if diff > largest_diff:
             largest_diff = diff
